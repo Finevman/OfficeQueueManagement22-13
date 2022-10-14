d566e01ab1ffa81af4adec9b3900e58d875cef1e
LeonardoVolpini <volpini.leonardo00@gmail.com>
2022-10-14 12:29:10 +0200

    crud user

--- server/DAO.js
+++ server/DAO.js
@@ -11,12 +11,10 @@ const db = new sqlite.Database('oqm.db', (err) => {
   }
 });
 
-/*************RIDDLES FUNCTIONS************/
-/*
-//readRiddles
-function readRiddles() {
+/*************userS FUNCTIONS************/
+function readUsers() {
   return new Promise((resolve, reject) => {
-    const sql = 'SELECT * FROM RIDDLES';
+    const sql = 'SELECT * FROM USERS';
     db.all(sql, (err, rows) => {
       if (err) {
         reject(err);
@@ -27,11 +25,10 @@ function readRiddles() {
   });
 }
 
-//addRiddle
-function addRiddle(riddle) {
+function addUser(user) {
   return new Promise((resolve, reject) => {
-    const sql = 'INSERT INTO RIDDLES (question, difficulty, duration, correct_answer, hint_1,hint_2,status_riddle,user_id) VALUES(?,?,?,?,?,?,?,?)';
-    db.run(sql, riddle.question, riddle.difficulty, riddle.duration, riddle.correct_answer, riddle.hint_1, riddle.hint_2, riddle.status_riddle, riddle.user_id, (err, rows) => {
+    const sql = 'INSERT INTO USERS (name, lastname, email, password, salt, role) VALUES(?,?,?,?,?,?)';
+    db.run(sql, user.name, user.lastname, user.email, user.password, user.salt, user.role, (err, rows) => {
       if (err) {
         reject(err);
       } else {
@@ -41,11 +38,11 @@ function addRiddle(riddle) {
   });
 }
 
-//updateRiddleStatus
-function updateRiddleStatus(id, status) {
+//update role
+function updateUserRole(id, role) {
   return new Promise((resolve, reject) => {
-    const sql = 'UPDATE riddles SET status_riddle = ? where id = ?';
-    db.run(sql, status, id, (err, rows) => {
+    const sql = 'UPDATE users SET role = ? where id = ?';
+    db.run(sql, role, id, (err, rows) => {
       if (err)
         reject(err);
       else
@@ -54,13 +51,26 @@ function updateRiddleStatus(id, status) {
   });
 }
 
+function deleteUser(id) {
+  return new Promise((resolve, reject) => {
+    const query = 'DELETE FROM USERS WHERE id = ?';
+    db.run(query, id, (err) => {
+      if (err) {
+        reject(err);
+      } else
+        resolve(true);
+    });
+  });
+};
 
+
+/*
 
 //getWinner
-function getWinner(riddle) {
+function getWinner(user) {
   return new Promise((resolve, reject) => {
-    const sql = 'SELECT name FROM USERS, ANSWERS WHERE id_user = id and id_riddle = ? and is_correct = 1';
-    db.get(sql, riddle, (err, rows) => {
+    const sql = 'SELECT name FROM USERS, ANSWERS WHERE id_user = id and id_user = ? and is_correct = 1';
+    db.get(sql, user, (err, rows) => {
       if (err)
         reject(err);
       else if (rows === undefined)
@@ -71,5 +81,5 @@ function getWinner(riddle) {
   })
 }
 
-module.exports = { readRiddles };
+module.exports = { readusers };
 */
