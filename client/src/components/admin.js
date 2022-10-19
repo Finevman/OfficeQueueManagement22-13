068e2f271bbe222d14527a5723a371b4bd8d5c85
LeonardoVolpini <volpini.leonardo00@gmail.com>
2022-10-19 12:19:44 +0200

    continue admin and user form

--- client/src/components/admin.js
+++ client/src/components/admin.js
@@ -3,12 +3,13 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { useState } from "react";
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import newUserForm from './newUserForm';
 
 function Admin(props) {
   return (
     <>
       <div style={{ fontSize: 45, width: "100%" }}>Admin Page</div>
-      <UserList users={props.users} changeRole={props.changeRole} />
+      <UserList users={props.users} changeRole={props.changeRole} addUser={props.addUser} deleteUser={props.deleteUser} />
     </>
   );
 }
@@ -34,11 +35,13 @@ function UserList(props) {  // list of user take by props
                 user={u}
                 key={u.code}
                 changeRole={props.changeRole}
+                deleteUser={props.deleteUser}
               />
             ))
           }
         </tbody>
-      </Table> </>
+      </Table>
+      <newUserForm addUser={props.addUser}/> </>
   );
 }
 
@@ -46,7 +49,7 @@ function UserRow(props) {
   return (
     <>
       <tr>
-        <UserActions user={props.user} changeRole={props.changeRole} />
+        <UserActions user={props.user} changeRole={props.changeRole} deleteUser={props.deleteUser} />
         <UserData user={props.user} />
       </tr>
     </>
@@ -72,13 +75,37 @@ function UserData(props) {
 function UserActions(props) {
   const [role, setRole] = useState(props.user.role);
   return (
-    <td className="col-3">
-      <DropdownButton id="dropdown-basic-button" title="Change Role" onSelect={ev => setRole(ev)}>
-        <Dropdown.Item eventKey="option-1">Officer</Dropdown.Item>
-        <Dropdown.Item eventKey="option-2">Manager</Dropdown.Item>
-      </DropdownButton>
-    </td>
+    <>
+      <td className="col-2">
+        <DropdownButton id="dropdown-basic-button" title="Change Role" onSelect={ev => setRole(ev)}>
+          <Dropdown.Item eventKey="option-1">Officer</Dropdown.Item>
+          <Dropdown.Item eventKey="option-2">Manager</Dropdown.Item>
+        </DropdownButton>
+      </td>
+      <td className="col-1">
+        <DeleteIcon onDelete={() => props.deleteUser(props.user)} />
+      </td>
+    </>
   );
 }
+
+const OPACITY_WHEN_MOUSE_INTERACT = 0.6;
+const NORMAL_OPACITY = 0.85;
+
+const DeleteIcon = (props) => {
+  const [opacity, setOpacity] = useState(NORMAL_OPACITY);
+  return (
+    <Trash3
+      fontSize={17}
+      style={{ marginBottom: 2 }}
+      onClick={props.onDelete}
+      opacity={opacity}
+      onMouseOver={() => setOpacity(OPACITY_WHEN_MOUSE_INTERACT)}
+      onMouseLeave={() => setOpacity(NORMAL_OPACITY)}
+      onMouseDown={() => setOpacity(1)}
+      onMouseUp={() => setOpacity(OPACITY_WHEN_MOUSE_INTERACT)}
+    />
+  );
+};
 
 export { Admin };
